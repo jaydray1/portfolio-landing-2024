b883a622778ae6a8975e2663b9f3923f099183fe
JM <jdreher15@gmail.com>
2024-06-12 16:26:58 -0600

    add content data tree.

---
+++ src/app/components/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+    <section>
+        <h6>Hello, I am</h6>
+        <h1>Jon-Michael</h1>
+        <p></p>
+    </section>
+}
+
+export default About
---
+++ src/app/content/sections-content.tsx
@@ -0,0 +1,49 @@
+export const landingIntro = {
+    helloLine: 'Hello, I am',
+    nameLine: 'Jon-Michael',
+    autoTypeLine: ['Architect, Mentor, Communicator', 'Coder', 'Engineer'],
+    landingIntroParagraph: {
+        text: `I collaborate, communicate, code, solve, architect, and lead. 
+            I've been working in the React ecosystem for the last six years where
+            What I've worked on:`,
+        lineItems: {
+            one: 'Redux, Context, Reducers, useState',
+            two: 'Styled-components, CSS, HTML, JSX',
+            three: 'React-hook-forms, custom hooks, Lodash, ES6, Draftjs'
+        },
+        buttonText: 'View Resume',
+        buttonLink: 'https://azure-donny-32.tiiny.site/'
+    },
+    landingImageSrc: 'static/img/_home-banner.jpg'
+}
+
+export const aboutSection = {
+    aboutHeadliner: `I'm a Frontend Engineer with over 6 years of experience.`,
+    aboutParagrapah: `I'm a Frontend Engineer with over six years of experience. 
+    I call the Wasatch Mountains of Salt Lake City home. I integrate seanlessly with motivated, 
+    intelligent, and passionate engineers and managers to bring ideas and products to
+     market and life quickly and efficiently. I tie my goals as an engineer into 
+     the goals of the companies I partner with so we can reap the benefits of help 
+     and success along with our customers and clients.`
+}
+
+export const experienceSection = [
+    {
+        ImageLink: '',
+        roleText: '',
+        companyAndDatesText: '',
+        description: ''
+    },
+    {
+        ImageLink: '',
+        roleText: '',
+        companyAndDatesText: '',
+        description: ''
+    },
+    {
+        ImageLink: '',
+        roleText: '',
+        companyAndDatesText: '',
+        description: ''
+    },
+]
